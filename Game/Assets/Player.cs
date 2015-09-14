17a8247cd942a15944a9c9b79caec0728f9b19e1
Liane <Liane.gaasendam@gmail.com>
2015-09-14 19:04:30 +0200

    Started off with 3d scene
    Added movement per key input, movementspeed, collision, movementdelay

---
+++ Game/Assets/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision) //er gebeurt collision
+    {
+        Debug.Log("hoi");
+    }
+
+    public float Movementspeed = 1; //de snelheid waarmee het object beweegt
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    public float m_WalkDelay = 0.1f;
+    private float m_WalkTimer = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_WalkTimer -= Time.deltaTime; // tel af naar 0
+
+        if (Input.GetKey("left") && m_WalkTimer <= 0.0f) //wat er moet gebeuren per toets wanneer de walktimer <=0 is
+        {
+            transform.Translate(Vector3.left * Movementspeed);
+            m_WalkTimer = m_WalkDelay;
+        }
+
+        if (Input.GetKey("right") && m_WalkTimer <= 0.0f)
+        {
+            transform.Translate(Vector3.right * Movementspeed);
+            m_WalkTimer = m_WalkDelay;
+        }
+
+        if (Input.GetKey("up") && m_WalkTimer <= 0.0f)
+        {
+            transform.Translate(Vector3.forward * Movementspeed);
+            m_WalkTimer = m_WalkDelay;
+        }
+
+        if (Input.GetKey("down") && m_WalkTimer <= 0.0f)
+        {
+            transform.Translate(Vector3.back * Movementspeed);
+            m_WalkTimer = m_WalkDelay;
+        }
+    }
+
+}
