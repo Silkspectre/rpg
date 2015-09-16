97758d9947a2117d87e4ee7b5474f782a28e9ed0
Liane <Liane.gaasendam@gmail.com>
2015-09-16 18:12:51 +0200

    Movement
    Added seeing the player move whenever a key is pressed

--- Game/Assets/Player.cs
+++ Game/Assets/Player.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+    public float Movementspeed = 1; //de snelheid waarmee het object beweegt
+    public float m_WalkDelay = 0.1f;
+
+    private float m_WalkTimer = 0.0f;
+    private Vector3 StartingPosition;
+    private Vector3 TargetPosition;
+
     void OnCollisionEnter(Collision collision) //er gebeurt collision
     {
    
@@ -20,46 +27,69 @@ public class Player : MonoBehaviour
         }
     }
 
-    public float Movementspeed = 1; //de snelheid waarmee het object beweegt
-
     // Use this for initialization
     void Start()
     {
         
     }
 
-    public float m_WalkDelay = 0.1f;
-    private float m_WalkTimer = 0.0f;
-
     // Update is called once per frame
     void Update()
     {
         m_WalkTimer -= Time.deltaTime; // tel af naar 0
 
-        if (Input.GetKey("left") && m_WalkTimer <= 0.0f) //wat er moet gebeuren per toets wanneer de walktimer <=0 is
+        //player niet teleporteert van 1 plek naar de andere, maar dat de beweging van de player naar de plek ook wordt getoond
+
+        // Maak een boolean die zegt of de speler aan het lopen is
+        // Dat doe je door te comtroleren of walktimer boven 0 is
+
+        bool PlayerWalking = m_WalkTimer >= 0;
+
+        // Als we niet aan het lopen zijn en als we een toets indrukken
+        // Zet de current position en de target position in CurrentPosition en TargetPosition
+
+        if (Input.GetKey("left") && PlayerWalking == false) //wat er moet gebeuren per toets wanneer de walktimer <=0 is
         {
-            transform.Translate(Vector3.left * Movementspeed);
+            // We moeten bewegen.
+            StartingPosition = transform.position;// De positie waar we nu staan (tijd 0%)
+            TargetPosition = StartingPosition + (Vector3.left * Movementspeed);// De positie waar we over 0.1 sec staan (tijd 100%)
             m_WalkTimer = m_WalkDelay;
         }
 
         if (Input.GetKey("right") && m_WalkTimer <= 0.0f)
         {
-            transform.Translate(Vector3.right * Movementspeed);
+            StartingPosition = transform.position;// De positie waar we nu staan (tijd 0%)
+            TargetPosition = StartingPosition + (Vector3.right * Movementspeed);// De positie waar we over 0.1 sec staan (tijd 100%)
             m_WalkTimer = m_WalkDelay;
         }
 
         if (Input.GetKey("up") && m_WalkTimer <= 0.0f)
         {
-            transform.Translate(Vector3.forward * Movementspeed);
+            StartingPosition = transform.position;// De positie waar we nu staan (tijd 0%)
+            TargetPosition = StartingPosition + (Vector3.forward * Movementspeed);// De positie waar we over 0.1 sec staan (tijd 100%)
             m_WalkTimer = m_WalkDelay;
         }
 
         if (Input.GetKey("down") && m_WalkTimer <= 0.0f)
         {
-            transform.Translate(Vector3.back * Movementspeed);
+            StartingPosition = transform.position;// De positie waar we nu staan (tijd 0%)
+            TargetPosition = StartingPosition + (Vector3.back * Movementspeed);// De positie waar we over 0.1 sec staan (tijd 100%)
             m_WalkTimer = m_WalkDelay;
         }
 
+        // Als de speler beweegt dan lerp tussen start en end position
+        if (PlayerWalking)
+        {
+            // begin = WalkingDelay = 0.1f == tijd op 0
+            // end = 0 == tijd op 1
+            
+            float tijd = (m_WalkDelay - Mathf.Max(0.0f, m_WalkTimer)) * (1.0f/m_WalkDelay);
+            transform.position = Vector3.Lerp(StartingPosition, TargetPosition, tijd);
+        }
+
+
+
+        //Translate
     }
         
 
