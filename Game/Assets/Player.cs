1b2161a029266729ef5a216c1177a6525f775818
Querijn Heijmans <querijn@irule.at>
2015-09-20 12:17:58 +0200

    Fixed up collision a bit

--- Game/Assets/Player.cs
+++ Game/Assets/Player.cs
@@ -12,21 +12,56 @@ public class Player : MonoBehaviour
 
     private bool m_CollidingWithNPC = false;
     private GameObject m_NPC = null;
+    private Vector3 m_ContactNormal = Vector3.zero;
 
-    void OnCollisionEnter(Collision collision) //er gebeurt collision
+    void OnCollisionStay(Collision collision)
     {
-   
+        m_WalkTimer = -1.0f;
+        
+        if (collision.contacts.Length>0)
+        {
+            int t_Count = 0;
+            foreach (ContactPoint contact in collision.contacts)
+            {
+                m_ContactNormal += contact.normal;
+                t_Count++;
+            }
+            m_ContactNormal /= t_Count;
+
+            //m_ContactNormal.Normalize();
+
+            transform.position += m_ContactNormal*0.01f;
+        }
+    }
+
+    void OnCollisionExit()
+    {
+
+    }
+
+    bool CanWalkInDirection(Vector3 a_Direction)
+    {
+        // De contactnormal is een genormaliseerde vector die wijst naar het object vanuit de muur
+        // als het verschil tussen de loopdirectie en de normal 2 is (squared), dan staan ze tegenover elkaar
+        // dus loop je tegen een contactpunt, dus mag je niet lopen.
+
+        // Mathf.Abs geeft een absolute waarde, alles onder nul wordt dezelfde waarde boven nul (-1 => 1, -333 => 333)
+        // Ik doe dit omdat floats onprecies zijn, soms wordt 0.0f iets als 0.000001f.
+        // 0.0f != 0.000001f, dus moet ik het absolute verschil hebben, waarbij ik controleer of het verschil minder dan 0.01f is.
+
+        if (Mathf.Abs((m_ContactNormal - a_Direction.normalized).sqrMagnitude-2*2) < 0.01f)
+            return false;
+
+        m_ContactNormal = Vector3.zero;
+        return true;
     }
 
     void OnTriggerEnter(Collider trigger)
     {
-        Debug.Log("Triggered with" + trigger.gameObject.name);
-
         //dit moet er nog voor zorgen dat je niet precies op het moment dat er collision is space hoeft te klikken
 
-        if (trigger.gameObject.name == "NPC")
+        if (trigger.gameObject.tag == "NPC")
         {
-            //Debug.Log("Hey, that NPC is colliding with us!");
             m_CollidingWithNPC = true;
             m_NPC = trigger.gameObject;
         }
@@ -34,12 +69,11 @@ public class Player : MonoBehaviour
 
     void OnTriggerExit(Collider trigger)
     {
-        if (trigger.gameObject.name == "NPC")
+        if (trigger.gameObject.tag == "NPC")
         {
-            //Debug.Log("We're no longer touching the NPC");
             m_CollidingWithNPC = false;
-            m_NPC = trigger.gameObject;
             m_NPC.GetComponent<Dialog>().EndConversation();
+            m_NPC = null;
         }
     }
 
@@ -66,35 +100,35 @@ public class Player : MonoBehaviour
         // Als we niet aan het lopen zijn en als we een toets indrukken
         // Zet de current position en de target position in CurrentPosition en TargetPosition
 
-        if (Input.GetKey("left") && PlayerWalking == false) //wat er moet gebeuren per toets wanneer de walktimer <=0 is
+        if (Input.GetKey("left") && PlayerWalking == false && CanWalkInDirection(Vector3.left)) //wat er moet gebeuren per toets wanneer de walktimer <=0 is
         {
             // We moeten bewegen.
             StartingPosition = transform.position;// De positie waar we nu staan (tijd 0%)
-            TargetPosition = StartingPosition + (Vector3.left * Movementspeed);// De positie waar we over 0.1 sec staan (tijd 100%)
+            TargetPosition = StartingPosition + Vector3.left;// De positie waar we over 0.1 sec staan (tijd 100%)
             m_WalkTimer = m_WalkDelay;
             m_CollidingWithNPC = false;
         }
 
-        if (Input.GetKey("right") && m_WalkTimer <= 0.0f)
+        if (Input.GetKey("right") && m_WalkTimer <= 0.0f && CanWalkInDirection(Vector3.right))
         {
             StartingPosition = transform.position;// De positie waar we nu staan (tijd 0%)
-            TargetPosition = StartingPosition + (Vector3.right * Movementspeed);// De positie waar we over 0.1 sec staan (tijd 100%)
+            TargetPosition = StartingPosition + Vector3.right;// De positie waar we over 0.1 sec staan (tijd 100%)
             m_WalkTimer = m_WalkDelay;
             m_CollidingWithNPC = false;
         }
 
-        if (Input.GetKey("up") && m_WalkTimer <= 0.0f)
+        if (Input.GetKey("up") && m_WalkTimer <= 0.0f && CanWalkInDirection(Vector3.forward))
         {
             StartingPosition = transform.position;// De positie waar we nu staan (tijd 0%)
-            TargetPosition = StartingPosition + (Vector3.forward * Movementspeed);// De positie waar we over 0.1 sec staan (tijd 100%)
+            TargetPosition = StartingPosition + Vector3.forward;// De positie waar we over 0.1 sec staan (tijd 100%)
             m_WalkTimer = m_WalkDelay;
             m_CollidingWithNPC = false;
         }
 
-        if (Input.GetKey("down") && m_WalkTimer <= 0.0f)
+        if (Input.GetKey("down") && m_WalkTimer <= 0.0f && CanWalkInDirection(Vector3.back))
         {
             StartingPosition = transform.position;// De positie waar we nu staan (tijd 0%)
-            TargetPosition = StartingPosition + (Vector3.back * Movementspeed);// De positie waar we over 0.1 sec staan (tijd 100%)
+            TargetPosition = StartingPosition + Vector3.back;// De positie waar we over 0.1 sec staan (tijd 100%)
             m_WalkTimer = m_WalkDelay;
             m_CollidingWithNPC = false;
         }
@@ -103,9 +137,10 @@ public class Player : MonoBehaviour
         if (PlayerWalking)
         {
             float tijd = (m_WalkDelay - Mathf.Max(0.0f, m_WalkTimer)); // ipv 0.1 tot 0.0 (walkdelay = 0.1) wordt het 0.0 tot 0.1
-            tijd = tijd * (1.0f/m_WalkDelay);                          // dan 1/0.1 = 10 en dat doe ik keer de tijd (0.0 tot 1.0)
+            tijd = tijd * (1.0f/m_WalkDelay) * Movementspeed;          // dan 1/0.1 = 10 en dat doe ik keer de tijd (0.0 tot 1.0)
 
             transform.position = Vector3.Lerp(StartingPosition, TargetPosition, tijd);
+
             // Linear interpolation van 0.0 tot 1.0 (tijd)
             // Kies een punt tussen StartinPosition en TargetPosition, gebaseerd op 'tijd'.
         }
