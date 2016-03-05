f999bdc7e58ce7bedf47073274cfc3bb818f4975
Querijn Heijmans <querijn@irule.at>
2016-03-05 14:23:41 +0100

    Fixed some errors

--- Hack/Assets/Scripts/Player.cs
+++ Hack/Assets/Scripts/Player.cs
@@ -4,35 +4,52 @@ using UnityEngine.Networking;
 
 public class Player : NetworkBehaviour
 {
-    Vector3 velocity = Vector3.zero;
+    Rigidbody2D RigidBody = null;
+    // Velocity is nu bepaald door de rigidbody, deze vector3 past nu dat aan.
+    public Vector3 velocity { get { return RigidBody.velocity; } set { RigidBody.velocity = value; } }
+
     public float speed = 10.0F;
     public float speedfalloff = 10.0F;
     public float MaxSpeed = 3.0F;
+    public float JumpStrength = 5.0F;
 
+    void Start ()
+    {
+        RigidBody = GetComponent<Rigidbody2D>();
+    }
 
-    // Use this for initialization
-    void Start () {
+    void OnCollisionEnter2D(Collision2D a_Collision)
+    {
+        if(a_Collision.collider.tag == "Player")
+        {
+            Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), a_Collision.collider);
+        }
+    }
 	
+	void FixedUpdate ()
+    {
+        // We can jump if our velocity is close to 0.
+        bool CanJump = Mathf.Abs(velocity.y) <= 0.03f;
+        
+        bool InTheAir = !CanJump;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
         var HorizontalInput = Input.GetAxis("Horizontal");
 
         if (Mathf.Abs(HorizontalInput)<0.03f)
         {
-            velocity -= velocity * Time.deltaTime * speedfalloff;
+            float t_NewXVelocity = velocity.x * Time.deltaTime * speedfalloff;
+            velocity = new Vector3(t_NewXVelocity, velocity.y);
         }
 
-        if (velocity.magnitude >= MaxSpeed)
+        if (Mathf.Abs(velocity.x) >= MaxSpeed)
         {
-            velocity = velocity.normalized * MaxSpeed;
+            float t_NewXVelocity = (velocity.x / Mathf.Abs(velocity.x)) * MaxSpeed;
+            velocity = new Vector3(t_NewXVelocity, velocity.y);
         }
 
-        if (Input.GetButton("Jump"))
+        if (CanJump && Input.GetButton("Jump"))
         {
-            velocity += Vector3.up;
+            velocity += Vector3.up * JumpStrength;
         }
 
         if (!isLocalPlayer)
@@ -42,7 +59,5 @@ public class Player : NetworkBehaviour
 
         //als laatste
         velocity += new Vector3(HorizontalInput, 0, 0) * speed * Time.deltaTime;
-        transform.position += velocity * Time.deltaTime;
-
     }
 }
