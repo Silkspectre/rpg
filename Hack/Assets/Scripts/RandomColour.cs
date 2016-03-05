fc78cbac07dc9c010aadc062fd68bcd634768870
Querijn Heijmans <querijn@irule.at>
2016-03-05 15:11:28 +0100

    Players now get a random colour assigned

---
+++ Hack/Assets/Scripts/RandomColour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class RandomColour : NetworkBehaviour
+{
+    Color RandomPastel()
+    {
+        Color t_Colour = Color.black;
+
+        int t_Colours = Random.Range(1, 3);
+        for(int i = 0; i < t_Colours; i++)
+        {
+            int t_Position;
+
+            do t_Position = Random.Range(0, 4); while (t_Colour[t_Position] != 0.0f);
+
+            t_Colour[t_Position] = 0.66f;
+        }
+
+        return t_Colour;
+    }
+
+
+	void Start ()
+    {
+	    if(isServer)
+        {
+            GetComponent<SpriteRenderer>().color = RandomPastel();
+        }
+	}
+
+	void Update ()
+    {
+	
+	}
+}
