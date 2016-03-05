5ad47a129ad604a7aaeb81b3edd69d1070681584
Liane <Liane.gaasendam@gmail.com>
2016-03-05 13:10:17 +0100

    Added movement
    movement velocity x axis

--- Hack/Assets/Player.cs
+++ Hack/Assets/Player.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class Player : MonoBehaviour {
+public class Player : NetworkBehaviour
+{
+    Vector3 velocity = Vector3.zero;
+    public float speed = 10.0F;
+    public float speedfalloff = 10.0F;
+    public float MaxSpeed = 3.0F;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
 	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        var HorizontalInput = Input.GetAxis("Horizontal");
+        var VerticalInput = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(HorizontalInput)<0.03f)
+        {
+            velocity -= velocity * Time.deltaTime * speedfalloff;
+        }
+
+        if (velocity.magnitude >= MaxSpeed)
+        {
+            velocity = velocity.normalized * MaxSpeed;
+        }
+
+        velocity += new Vector3(HorizontalInput, 0, 0) * speed * Time.deltaTime;
+
+        transform.position += velocity * Time.deltaTime;
+    }
 }
